7baf39bd1be9539cb22584978f578184efd6fc7e
simrangit786 <59757463+simrangit786@users.noreply.github.com>
2021-04-23 19:11:57 -0700

    updation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppsComponent } from './apps/apps.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppsComponent
   ],
   imports: [
     BrowserModule
