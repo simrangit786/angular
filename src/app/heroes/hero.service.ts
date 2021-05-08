6ce5c1bd6113b30acc7081daceaa6a3c7abfdb22
simrangit786 <59757463+simrangit786@users.noreply.github.com>
2021-05-07 17:19:28 -0700

    service updation

---
+++ src/app/heroes/hero.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { HEROES } from './mock-heroes';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  getHeroes() { return HEROES; }
+}
---
+++ src/app/heroes/heroes-tsp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-heroes-tsp',
+    template: `
+      <h2>Heroes</h2>
+      <app-hero-list></app-hero-list>
+    `
+  })
+  export class HeroesTspComponent { }
